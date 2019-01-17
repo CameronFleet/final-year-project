6fe73aab02d442e91df3db158ef135b6222829e5
CameronFleet1998 <Cameron-Fleet@hotmail.com>
2019-01-17 11:54:55 +0000

    Added constants defining goal width

--- config.py
+++ config.py
@@ -1,23 +1,26 @@
 FPS = 50
 SCALE = 30.0  # affects how fast-paced the game is, forces should be adjusted as well
 
-MAIN_ENGINE_POWER = 13.0
-SIDE_ENGINE_POWER = 0.6
+MAIN_ENGINE_POWER = 20
+SIDE_ENGINE_POWER = 2
+
+GOAL_HEIGHT_SCALAR = 0.3
+GOAL_WIDTH_SCALAR = (0.4,0.6)
 
 INITIAL_RANDOM = 1000.0  # Set 1500 to make game harder
 
 LANDER_POLY = [
-    (-14, +17), (-17, 0), (-17, -10),
-    (+17, -10), (+17, 0), (+14, +17)
+    (-10, +190), (-10, 0), (0, 210),
+    (+10, +190), (+10, 0),
 ]
 
 LEG_AWAY = 20
 LEG_DOWN = 18
-LEG_W, LEG_H = 2, 12
+LEG_W, LEG_H = 4, 24
 LEG_SPRING_TORQUE = 40
 
 SIDE_ENGINE_HEIGHT = 14.0
 SIDE_ENGINE_AWAY = 12.0
 
-VIEWPORT_W = 600
-VIEWPORT_H = 400
+VIEWPORT_W = 1300
+VIEWPORT_H = 800
