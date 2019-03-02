7ebe78057c1023062788ed369db1c5e919700ad9
CameronFleet1998 <Cameron-Fleet@hotmail.com>
2019-03-02 15:42:01 +0000

    Cleaned up BoosterAgent changes

--- environment/boosteragent.py
+++ environment/boosteragent.py
@@ -6,25 +6,25 @@ class BoosterAgent:
     def __init__(self, world, W, H, np_random):
         self.body = builder.generate_booster(world, W, H, np_random)
         self.np_random = np_random
-        self.MAIN_ENGINE_POWER = config.MAIN_ENGINE_POWER
+        self.MAIN_ENGINE_POWER = config.MAIN_ENGINE_POWER * 1/config.FPS
+        self.SIDE_ENGINE_POWER = config.SIDE_ENGINE_POWER * 1/config.FPS
 
     # Actions
     def fireMainEngine(self, m_power, alpha, create_particle, record_metrics):
 
-        tip, side, dispersion = self.calcMetrics()
-
+        dispersion = self.np_random.uniform(-0.1, +0.1)
         impulse_pos = (self.body.position[0], self.body.position[1])
 
         if self.MAIN_ENGINE_POWER:
             p = create_particle(3.5, impulse_pos[0], impulse_pos[1], m_power)  # particles are just a decoration, 3.5 is here to make particle speed adequate
             p.coldGas = False  
 
-            angle = -(self.body.angle +alpha + dispersion[0])
+            angle = -(self.body.angle +alpha + dispersion)
             Ft = self.MAIN_ENGINE_POWER * m_power 
             Fy = Ft*math.cos(angle)
             Fx = Ft*math.sin(angle)
 
-            record_metrics(Fx, Fy)
+            record_metrics({"Fx":Fx, "Fy":Fy, "Ft":math.sqrt(Fx*Fx + Fy*Fy)})
 
             p.ApplyLinearImpulse((-Fx, -Fy), impulse_pos,True)
             self.body.ApplyLinearImpulse((Fx, Fy),
@@ -32,8 +32,8 @@ class BoosterAgent:
 
 
     def fireSideEngine(self, s_power, direction, create_particle):
-        tip, side, dispersion = self.calcMetrics()
 
+        dispersion = self.np_random.uniform(-0.1, +0.1)
         impulse_pos = (self.body.position[0] 
                             - config.SIDE_ENGINE_HEIGHT*math.sin(self.body.angle)
                             + direction*(config.SIDE_ENGINE_AWAY/2)*math.cos(self.body.angle), 
@@ -41,23 +41,19 @@ class BoosterAgent:
                             + config.SIDE_ENGINE_HEIGHT*math.cos(self.body.angle)
                             + direction*(config.SIDE_ENGINE_AWAY/2)*math.sin(self.body.angle))
 
-        if config.SIDE_ENGINE_POWER:
+        if self.SIDE_ENGINE_POWER:
             p = create_particle(0.7, impulse_pos[0], impulse_pos[1], s_power)
             p.coldGas = True
 
-            angle = self.body.angle + dispersion[0] 
-            Fs = direction*config.SIDE_ENGINE_POWER * s_power
+            angle = self.body.angle + dispersion 
+            Fs = direction*self.SIDE_ENGINE_POWER * s_power
             Fx = Fs*math.cos(angle)
             Fy = Fs*math.sin(angle)
 
             p.ApplyLinearImpulse((Fx, Fy), impulse_pos, True)
             self.body.ApplyLinearImpulse((-Fx, -Fy), impulse_pos, True)
 
-    def calcMetrics(self): 
-        tip = (math.sin(self.body.angle), math.cos(self.body.angle))
-        side = (-tip[1], tip[0])
-        dispersion = [self.np_random.uniform(-0.1, +0.1) for _ in range(2)]
-        return tip, side, dispersion
+
 
 
 
